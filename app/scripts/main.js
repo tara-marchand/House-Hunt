d506613368220dc205c3a8fed510c5b762ba0af4
Tara Marchand <tara@mac.com>
2013-01-18 14:22:09 -0800

    Canvas area rollovers

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,15 +1,4 @@
-/**
- *
- * Model: HousePreferences
- * View: HousePreferencesView
- *
- * Model: Neighborhood
- * View: NeighborhoodView
- *
- * Collection: AllNeighborhoods
- * View: AllNeighborhoodsView
- *
- */
+// http://zreference.com/image-map-canvas/
 
 $(document).ready(function() {
     "use strict";
@@ -18,29 +7,57 @@ $(document).ready(function() {
 
 var hh = (function() {
     "use strict";
-    var hh = {},
-    housePrefsView = null;
+    var hh = {};
+    hh.housePrefsView = null;
 
-    hh.HousePreferences = Parse.Object.extend({
+    var HousePreferences = Parse.Object.extend({
         className: "HousePreferences",
         initialize: function() {
             this.on("change:form", this.save);
         }
     });
 
-    hh.HousePreferencesView = Parse.View.extend({
+    var HousePreferencesView = Parse.View.extend({
         model: null,
 
         events: {
             "change select, change input": "changeForm",
-            "change input:radio": "changeForm"
+            "change input:radio": "changeForm",
+            "mouseover area": "setCanvasAreaStyle",
+            "mouseout area": "setCanvasAreaStyle",
+            "click area": "setCanvasAreaStyle"
         },
 
         initialize: function() {
+            this.$map = $("#map");
+            this.$areas = $("#areas");
+
+            this.$canvas = $("#canvas")[0];
+            this.$canvas.width = 800;
+            this.$canvas.height = 724;
+
+            this.context = this.$canvas.getContext("2d");
+            this.fillStyleOff = "white";
+            this.fillStyleOver = "red";
+            this.fillStyleOn = "blue";
+            this.strokeStyleOff = "grey";
+            this.strokeStyleOver = "blue";
+            this.strokeStyleOn = "red";
+
+           this.context.lineWidth = 2;
+
             this.render();
         },
 
         render: function() {
+            this.renderForm();
+            this.clearCanvas();
+            this.context.fillStyle = this.fillStyleOff;
+            this.context.strokeStyle = this.strokeStyleOff;
+            this.renderCanvas();
+        },
+
+        renderForm: function() {
             var selectedNeighborhoods = this.model.get("neighborhoods"),
             neighborhoodsOptions = $("#neighborhoods").find("option"),
             neighborhoodsOptionsLength = neighborhoodsOptions.length,
@@ -66,14 +83,12 @@ var hh = (function() {
             $(garageSelector).attr("checked", true);
             $("#squarefeet").find(squarefeetSelector).attr("selected", "selected");
             $("#price").find(priceSelector).attr("selected", "selected");
-
         },
 
         changeForm: function(e) {
             var modelUpdates = {},
             el = e.target,
             $el = $(el),
-            elNodeName = el.nodeName,
             attrib = "",
             val = "";
 
@@ -88,124 +103,83 @@ var hh = (function() {
             modelUpdates[attrib] = val;
             this.model.set(modelUpdates);
             this.model.trigger("change:form");
+        },
+
+       fillAndStrokeCanvas: function(){
+            if (this.context.fillStyle) {
+                this.context.fill();
+            }
+            if (this.context.strokeStyle) {
+                this.context.stroke();
+            }
+        },
+
+        clearCanvas: function() {
+            this.context.fillStyle = "transparent";
+            this.context.fillRect(0, 0, this.canvasWidth, this.canvasHeight);
+        },
+
+        renderCanvasArea: function(area) {
+            var $area = $(area);
+            var shape = $area.attr("shape");
+            var coords = $area.attr("coords").split(",");
+
+            this.context.fillStyle = ($area.data("fillStyle")) ? $area.data("fillStyle") : this.fillStyleOff;
+            this.context.strokeStyle = ($area.data("strokeStyle")) ? $area.data("strokeStyle") : this.strokeStyleOff;
+            this.context.lineWidth = ($area.data("lineWidth")) ? $area.data("lineWidth") : this.lineWidth;
+
+            this.context.beginPath();
+            var leng = coords.length;
+            this.context.moveTo(coords[0], coords[1]);
+            for (var j = 2; j < leng; j += 2){
+                this.context.lineTo(coords[j], coords[j+1]);
+            }
+            this.context.closePath();
+            this.fillAndStrokeCanvas();
+        },
+
+        setCanvasAreaStyle: function(e) {
+            var eventType = e.originalEvent.type,
+                el = e.target,
+                $el = $(el);
+            console.log(eventType);
+            if (eventType === "mouseover") {
+                $el.data({
+                    fillStyle: this.fillStyleOver,
+                    strokeStyle: this.strokeStyleOver
+                });
+            } else if (eventType === "mouseout") {
+                $el.data({
+                    fillStyle: this.fillStyleOff,
+                    strokeStyle: this.strokeStyleOff
+                });
+            }
+            this.renderCanvasArea(el);
+        },
+
+        renderCanvas: function() {
+            var areasChildren = this.$areas.children(),
+            areasChildrenLength = areasChildren.length;
+
+            for (var i = 0; i < areasChildrenLength; i++) {
+                this.renderCanvasArea(areasChildren[i]);
+            }
+
         }
+
     });
 
-    var addEventHandlers = function() {
-        // areas
-        $("area").on("mouseenter", function() {
-            alert($(this).data("value"));
-        });
-    };
-
-    hh.init = function() {
+   hh.init = function() {
         Parse.initialize("8De6SQMWtbOWrok19a0JA5I7SANT6FQP5a85WEy6", "bWAJd39x8c63Lv8xuKcafgu4TrbAqRqIr3Z1XayZ");
-        var housePrefsQuery = new Parse.Query(hh.HousePreferences);
+        var housePrefsQuery = new Parse.Query(HousePreferences);
         housePrefsQuery.get("xqDFn4ZkLt", {
             success: function(results) {
-                housePrefsView = new hh.HousePreferencesView({
-                    el: "form",
+                hh.housePrefsView = new HousePreferencesView({
+                    el: "#container",
                     model: results
                 });
-                addEventHandlers();
             }
         });
-
-
-
-
-
-
-
-
-
-
-
-
-        var TWO_PI = Math.PI * 2;
-
-        var map = $("#map");
-        var areas = $("#areas");
-        var canvas = $("#canvas")[0];
-        canvas.width = 800;
-        canvas.height = 724;
-        var c = canvas.getContext("2d");
-
-        var fillStyle = "#bf9c82";
-        var strokeStyle = "#ebce59";
-        var lineWidth = 2;
-
-        $.fn.render = function(){ 
-            this.data.apply(this, arguments);
-            render();
-        };
-
-        function clear(){    
-            c.fillStyle = "transparent";
-            c.fillRect(0, 0, canvas.width, canvas.height);
-        }
-
-        $('area').mouseenter(function(){
-            $(this).render();
-            render();
-        }).mouseleave(function(){
-            $(this).render();
-
-        });
-
-        render();
-
-        function setStyle(){
-            if (fillStyle){
-                c.fillStyle = fillStyle; 
-            }
-            if (strokeStyle){
-                if (lineWidth){
-                    c.lineWidth = lineWidth; 
-                }
-                c.strokeStyle = strokeStyle;   
-            }
-        }
-
-        function fillStroke(){
-            if (fillStyle) c.fill();
-            if (strokeStyle) c.stroke();
-        }
-
-        function render(noClear){
-            if (!noClear){
-                clear();
-            }
-
-            areas.children().each(function(i){
-                var area = $(this);
-                var shape = area.attr("shape");
-                var coords = area.attr("coords").split(",");
-
-                if (fillStyle){
-                    c.fillStyle = fillStyle; 
-
-                }
-                if (strokeStyle){
-                    if (lineWidth){
-                        c.lineWidth = lineWidth; 
-                    }
-                    c.strokeStyle = strokeStyle;   
-                }
-
-                c.beginPath();
-                var leng = coords.length;
-                c.moveTo(coords[0], coords[1]);
-                for (var i = 2; i < leng; i+=2){
-                    c.lineTo(coords[i], coords[i+1]); 
-                }
-                c.closePath();
-
-                fillStroke(fillStyle, strokeStyle);
-
-                c.lineWidth = 1;
-            });
-        }
 
     };
 
